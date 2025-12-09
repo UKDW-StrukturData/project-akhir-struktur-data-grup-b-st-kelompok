2f858ca5b3b14d4775a50cf4cd2e107c4938e644
intanaryani <71241122@students.ukdw.ac.id>
2025-12-09 10:01:22 +0700

    Week 14 (7)

--- page_bookmark.py
+++ page_bookmark.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
 def page_bookmark():
     st.title("Bookmark")
-    st.info("masih kosong")
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    data = conn.read(worksheet="bookmarks", ttl=0)
+    df = pd.DataFrame(data)
+
+    st.dataframe(df)
